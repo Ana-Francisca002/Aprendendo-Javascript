40a00fe394d931937be878373eef39aef588f86d
Ana Francisca <anafrancisca.dev@gmail.com>
2024-09-27 08:38:37 -0300

    Pequenas alterações

--- 3.Estruturas-Controle/3.If-ElseIf.js
+++ 3.Estruturas-Controle/3.If-ElseIf.js
@@ -21,7 +21,3 @@ ImprimirResultados(10)
 ImprimirResultados(0)
 ImprimirResultados(5)
 ImprimirResultados(7)
-
-/*Podemos caracterizar, desta forma, o prototype como sendo um método ou um construtor de classes (em nosso caso Objetos JavaScript “JSON”) para executar códigos ou até mesmo criar novos objetos.
-
-A partir do prototype podemos atribuir funções e executar procedimentos aninhados ou não a partir de um objeto criado ao qual conterá as informações que poderemos utilizar durante o código. */
