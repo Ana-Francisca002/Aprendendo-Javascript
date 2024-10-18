8caa1233b56a045a4381d28b75d501c140174dc2
Ana Francisca <anafrancisca.dev@gmail.com>
2024-10-18 09:42:07 -0300

    Errei a desgraça no commit anterior, é mesma coisa

---
+++ 4.Função/4.Param-Padrão.js
@@ -0,0 +1,33 @@
+//Essas estratégias são para que as funções retornem valores padrão 
+
+// Estrátegia 1
+
+function soma1 (a, b, c){
+    a = a || 1
+    b = b || 1
+    c = c || 1
+
+    return a + b + c
+}
+
+console.log(soma1(1), soma1(3), soma1(1, 2, 3), soma1(0, 0, 0))
+
+//Estratégia 2, 3 e 4
+
+function soma2 (a, b, c){
+    a = a !== undefined || 1
+    b = 1 in arguments ? b : 1
+    c = isNaN(c) ? 1 : c
+
+    return a + b + c
+}
+
+console.log(soma2(1), soma2(3), soma2(1, 2, 3), soma2(0, 0, 0))
+
+//ES 2015 - essa é a estratégia mais recomendada
+
+function soma3 (a = 1, b = 1, c = 1){
+    return a + b + c
+}
+
+console.log(soma3(1), soma3(3), soma3(1, 2, 3), soma3(0, 0, 0))
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Curso web moderna
+# Aprendendo javascript para deixar de ser burra
 
 Repositório destinado aos meus estudos de javascript com vários materiais que tenho disponível, desde: curso web moderna, livros e muitos exercícios na neps academy e codewars. 
